a6ea9b0a07c308d4cd5c466d4f99684dfb143852
Cambelau <sajotmatthieu@gmail.com>
2021-10-13 10:08:27 +0200

    add lab5

---
+++ lab/lab5/assets/userapi/src/controllers/user.js
@@ -0,0 +1,23 @@
+const client = require('../dbClient')
+
+module.exports = {
+  create: (user, callback) => {
+    // Check parameters
+    if(!user.username)
+      return callback(new Error("Wrong user parameters"), null)
+    // Create User schema
+    const userObj = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    }
+    // Save to DB
+    // TODO check if user already exists
+    client.hmset(user.username, userObj, (err, res) => {
+      if (err) return callback(err, null)
+      callback(null, res) // Return callback
+    })
+  },
+  // get: (username, callback) => {
+  //   // TODO create this method
+  // }
+}
